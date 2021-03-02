1ee93db278e72d00aeaae60249b2ba48aa48800b
Shafi <adam.shafi@capgemini.com>
2021-03-02 16:02:25 +0000

    pickles

--- app_demo.py
+++ app_demo.py
@@ -33,14 +33,27 @@ stqdm.pandas()
 @st.cache(allow_output_mutation=True)
 def load_models():
     a = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
-    b = SentenceTransformer('stsb-distilbert-base')
-    c = SentenceTransformer('stsb-roberta-large') 
-    d = SentenceTransformer('msmarco-distilbert-base-v2')
-    return a,b,c,d       
+    try:
+        b = pickle.load(open('./models/dbert.pkl', 'rb'))
+    except:
+        b = SentenceTransformer('stsb-distilbert-base')
+        pickle.dump(b, open('../demo_app/models/dbert.pkl', 'wb'))
+    try:
+        c = 'na'#pickle.load(open('./models/rbert.pkl', 'rb'))
+    except:
+        b = SentenceTransformer('stsb-roberta-large')
+        pickle.dump(b, open('../demo_app/models/rbert.pkl', 'wb'))       
+    try:
+        d = pickle.load(open('./models/qbert.pkl', 'rb'))
+    except:
+        b = SentenceTransformer('msmarco-distilbert-base-v2')
+        pickle.dump(b, open('../demo_app/models/qbert.pkl', 'wb'))       
+    return a,b,c,d   
+
 
 @st.cache(hash_funcs={preshed.maps.PreshMap:id, cymem.cymem.Pool:id}, allow_output_mutation=True)#hash_funcs={preshed.maps.PreshMap: lambda x: 1, cymem.cymem.Pool:      lambda x: 1})
 def load_summarizer():
-    return Summarizer()
+    return pickle.load(open('./models/summarizer.pkl', 'rb'))
 
 @st.cache()
 def load_pdf(file)->str:
@@ -110,6 +123,29 @@ def summarize(text, model, n=1):
 def get_embeddings(embeddings_option):
     return pd.read_pickle(options[embeddings_option][0])
 
+from transformers import TFGPT2LMHeadModel, GPT2Tokenizer
+import tensorflow as tf
+model_gpt = TFGPT2LMHeadModel.from_pretrained('distilgpt2')
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+
+def ab_sum(q,t):
+    in_text = t+' '+q
+    input_ids = tokenizer.encode(in_text, return_tensors="tf")
+    max_len = input_ids.shape[1]
+    start = len(in_text)
+    generated_text_samples = model_gpt.generate(
+                                            input_ids, 
+                                            max_length=max_len+20,  
+                                            num_return_sequences=1,
+                                            no_repeat_ngram_size=2,
+                                            repetition_penalty=1.5,
+                                            top_p=0.92,
+                                            temperature=.85,
+                                            do_sample=True,
+                                            top_k=125,
+                                            early_stopping=True)
+    return tokenizer.decode(generated_text_samples[0], skip_special_tokens=True)[start:]
+
 ### APP
 st.title('BERT Passage Scoring')
 
@@ -117,10 +153,10 @@ st.title('BERT Passage Scoring')
 use,dbert,rbert,qbert = load_models()
 
 
-options = {'Universal Sentence Encoder': ['use.pkl',use],
-           'DistillBERT':['distilbert.pkl',dbert], 
-           'RoBERTa Large':['robert.pkl',rbert],
-           'DistillBERT Q&A':['distilbertqa.pkl',qbert],
+options = {'Universal Sentence Encoder': ['./embeddings/use.pkl',use],
+           'DistillBERT':['./embeddings/distilbert.pkl',dbert], 
+           'RoBERTa Large':['./embeddings/robert.pkl',rbert],
+           'DistillBERT Q&A':['./embeddings/distilbertqa.pkl',qbert],
            'Select a model...':['','']}
 
 selectbox_list = list(options.keys())
@@ -137,6 +173,7 @@ s = get_articles(text)
 embeddings_option = st.selectbox('Which model?', selectbox_list, index=4)
 q = st.text_input('What is your query?')
 
+
 if q:
     X = get_embeddings(embeddings_option)
     ans = ask(q, X=X, s=s, n=3, model=options[embeddings_option][1],embeddings_option=embeddings_option)
@@ -144,16 +181,13 @@ if q:
         with st.beta_expander(f'ARTICLE {t.split()[0]}'):
             if len(t.split('. '))>3:
                 summary = summarize(t, summarizer_model, 1)
+                ab_summary = ab_sum(q,t)
                 st.success(summary)
+               #t = ". ".join([f"**{sent}**" if sent in summary.split(". ") else sent for sent in t.split(". ")])   
+            else: 
+                ab_summary=None
             st.write(t)
-    
-        
-
-
-
-
-
-
-
+            if ab_summary:
+                st.warning(ab_summary)
 
 
